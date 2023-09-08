784f9223fbbe585fc2b5b0f496fffc2c147fbf16
bg yogenther <yogisanjay2522@gmail.com>
2023-09-08 13:24:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+//TOGGLE
+
+const movieLists = document.querySelectorAll(".movie-list");
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+  ".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle"
+);
+
+ball.addEventListener("click", () => {
+  items.forEach((item) => {
+    item.classList.toggle("active");
+  });
+  ball.classList.toggle("active");
+});
